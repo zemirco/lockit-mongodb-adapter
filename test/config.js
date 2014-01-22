574e695cdc4f6e5b4a06fe3b5b2f3962ad7d84fc
zeMirco <mirco.zeiss@gmail.com>
2014-01-22 20:43:47 +0100

    add signup token expiration to config

--- test/config.js
+++ test/config.js
@@ -1,3 +1,6 @@
-exports.db = 'mongodb';
-exports.dbUrl = 'mongodb://127.0.0.1/test';
+exports.db = 'mongodb://127.0.0.1/test';
 exports.dbCollection = 'users';
+
+exports.signup = {
+  tokenExpiration: '1 day'
+};
