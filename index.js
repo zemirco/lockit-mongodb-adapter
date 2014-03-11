3393ce727c5fff9ffe77814e39151a6fdcd8c38d
zeMirco <mirco.zeiss@gmail.com>
2014-03-11 21:59:04 +0100

    remove debug

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var uuid = require('node-uuid');
 var bcrypt = require('bcrypt');
 var ms = require('ms');
 var moment = require('moment');
-var debug = require('debug')('lockit-mongodb-adapter');
 
 module.exports = function(config) {
 
@@ -12,7 +11,6 @@ module.exports = function(config) {
   var db;
   MongoClient.connect(config.db, function(err, database) {
     if (err) throw err;
-    debug('connected to MongoDB');
     db = database;
   });
 
@@ -40,10 +38,7 @@ module.exports = function(config) {
     bcrypt.hash(pw, 10, function(err, hash) {
       if (err) return done(err);
       user.hash = hash;
-
-      debug('New user created: %j', user);
       db.collection(config.dbCollection).save(user, done);
-
     });
 
   };
@@ -54,7 +49,6 @@ module.exports = function(config) {
     var qry = {};
     qry[match] = query;
 
-    debug('finding user with %s "%s"', match, query);
     db.collection(config.dbCollection).find(qry).nextObject(done);
 
   };
@@ -67,7 +61,6 @@ module.exports = function(config) {
       if (err) console.log(err);
 
       // res is not the updated user object! -> find manually
-      debug('updating user: %j', user);
       db.collection(config.dbCollection).find({_id: user._id}).nextObject(done);
 
     });
@@ -83,7 +76,6 @@ module.exports = function(config) {
     db.collection(config.dbCollection).remove(qry, function(err, numberOfRemovedDocs) {
       if (err) return done(err);
       if (numberOfRemovedDocs === 0) return done(new Error('lockit - Cannot find ' + match + ': "' + query + '"'));
-      debug('user removed from db');
       done(null, true);
     });
 
