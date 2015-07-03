bafce7665fd01540ca92a151d1a60411a446a0cd
Mirco Zeiss <mirco.zeiss@gmail.com>
2015-07-03 13:50:20 +0200

    update dependencies

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+'use strict';
 
 var should = require('should');
 var config = require('./config.js');
@@ -20,7 +21,7 @@ describe('lockit-mongodb-adapter', function() {
 
   it('should create a new user', function(done) {
     adapter.save('john', 'john@email.com', 'secret', function(err, res) {
-      if (err) console.log(err);
+      if (err) {console.log(err); }
       res.should.have.property('signupToken');
       res.signupToken.should.match(/[0-9a-f]{22}|[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
       res.should.have.property('failedLoginAttempts');
@@ -33,7 +34,7 @@ describe('lockit-mongodb-adapter', function() {
 
   it('should find a user by name', function(done) {
     adapter.find('name', 'john', function(err, res) {
-      if (err) console.log(err);
+      if (err) {console.log(err); }
       res.name.should.equal('john');
       res.email.should.equal('john@email.com');
       done();
@@ -42,7 +43,7 @@ describe('lockit-mongodb-adapter', function() {
 
   it('should return undefined when no user is found', function(done) {
     adapter.find('name', 'jim', function(err, res) {
-      if (err) console.log(err);
+      if (err) {console.log(err); }
       should.not.exist(err);
       should.not.exist(res);
       done();
@@ -51,7 +52,7 @@ describe('lockit-mongodb-adapter', function() {
 
   it('should find a user by email', function(done) {
     adapter.find('email', 'john@email.com', function(err, res) {
-      if (err) console.log(err);
+      if (err) {console.log(err); }
       res.name.should.equal('john');
       res.email.should.equal('john@email.com');
       done();
@@ -60,7 +61,7 @@ describe('lockit-mongodb-adapter', function() {
 
   it('should find a user by signup token', function(done) {
     adapter.find('signupToken', _tmp_signupToken, function(err, res) {
-      if (err) console.log(err);
+      if (err) {console.log(err); }
       res.name.should.equal('john');
       res.email.should.equal('john@email.com');
       done();
@@ -69,11 +70,11 @@ describe('lockit-mongodb-adapter', function() {
 
   it('should update an existing user', function(done) {
     adapter.find('name', 'john', function(err, doc) {
-      if (err) console.log(err);
+      if (err) {console.log(err); }
       doc.test = 'works';
       doc.editet = true;
-      adapter.update(doc, function(err, res) {
-        if (err) console.log(err);
+      adapter.update(doc, function(updateErr, res) {
+        if (updateErr) {console.log(updateErr); }
         res.test.should.equal('works');
         res.editet.should.be.true;
         done();
@@ -82,10 +83,10 @@ describe('lockit-mongodb-adapter', function() {
   });
 
   it('should remove a user', function(done) {
-    adapter.save('jeff', 'jeff@email.com', 'secret', function(err, res) {
-      if (err) console.log(err);
-      adapter.remove('jeff', function(err, res) {
-        if (err) console.log(err);
+    adapter.save('jeff', 'jeff@email.com', 'secret', function(err) {
+      if (err) {console.log(err); }
+      adapter.remove('jeff', function(removeErr, res) {
+        if (removeErr) {console.log(removeErr); }
         res.should.be.true;
         done();
       });
@@ -93,7 +94,7 @@ describe('lockit-mongodb-adapter', function() {
   });
 
   it('should return an error when remove cannot find a user', function(done) {
-    adapter.remove('steve', function(err, res) {
+    adapter.remove('steve', function(err) {
       err.message.should.equal('lockit - Cannot find user "steve"');
       done();
     });
